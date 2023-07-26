ec22444d2d1f70a230f65ff859031d82e6929603
Kashyap Chaganti <Kashyapchaganti96@gmail.com>
2023-07-25 21:35:00 -0400

    Added Event Drop Down Option

--- producer.js
+++ producer.js
@@ -1,3 +1,50 @@
+// const kafka = require('kafka-node');
+
+// const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+// const producer = new kafka.Producer(client);
+
+// const topic = 'streaming-data';
+
+// producer.on('ready', () => {
+//   console.log('Kafka producer is ready');
+
+//   // Simulate sending data every half second
+//   setInterval(sendData, 100);
+// });
+
+// producer.on('error', (error) => {
+//   console.error('Error in Kafka producer:', error);
+// });
+
+// function sendData() {
+//   const message = generateMessage();
+//   const payload = [
+//     {
+//       topic: topic,
+//       messages: JSON.stringify(message),
+//     },
+//   ];
+
+//   producer.send(payload, (error, data) => {
+//     if (error) {
+//       console.error('Error sending message:', error);
+//     } else {
+//       console.log('Message sent:', data);
+//     }
+//   });
+// }
+
+// function generateMessage() {
+//   // Generate sample data (replace this with your real data source)
+//   const randomPower = Math.random() * 100;
+//   const timestamp = new Date().toISOString();
+
+//   return {
+//     timestamp: timestamp,
+//     data: randomPower,
+//   };
+// }
+
 const kafka = require('kafka-node');
 
 const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
@@ -5,19 +52,23 @@ const producer = new kafka.Producer(client);
 
 const topic = 'streaming-data';
 
+const events = ['Event 1', 'Event 2', 'Event 3']; // List of events
+
 producer.on('ready', () => {
   console.log('Kafka producer is ready');
 
-  // Simulate sending data every second
-  setInterval(sendData, 1000);
+  // Simulate sending data every half second for each event
+  events.forEach((event) => {
+    setInterval(() => sendData(event), 1000);
+  });
 });
 
 producer.on('error', (error) => {
   console.error('Error in Kafka producer:', error);
 });
 
-function sendData() {
-  const message = generateMessage();
+function sendData(event) {
+  const message = generateMessage(event);
   const payload = [
     {
       topic: topic,
@@ -34,7 +85,7 @@ function sendData() {
   });
 }
 
-function generateMessage() {
+function generateMessage(event) {
   // Generate sample data (replace this with your real data source)
   const randomPower = Math.random() * 100;
   const timestamp = new Date().toISOString();
@@ -42,5 +93,6 @@ function generateMessage() {
   return {
     timestamp: timestamp,
     data: randomPower,
+    event: event,
   };
 }
